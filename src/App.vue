5f02d8812d7a24187c04e19a7fb627709779bb59
Elizaveta Vinokurova <de.spring@gmail.com>
2022-06-04 20:24:33 +0300

    basic scaffolding

--- src/App.vue
+++ src/App.vue
@@ -2,7 +2,11 @@
 </script>
 
 <template>
-  <img alt="Vue logo" src="./assets/logo.png" />
+  <router-link to="/">Home</router-link> |
+  <router-link to="/about">About</router-link>
+
+<!--  ↓ отрисует компонент, соответсвующий заданному path -->
+  <router-view />
 </template>
 
 <style>
